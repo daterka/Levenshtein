860d2313b54df6def154186e660955df03533d36
dan.te <fallowdeer.mail@gmail.com>
2020-03-20 11:58:50 +0100

    -streaming through the file

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -1,28 +1,47 @@
 package com.company;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Client {
 
-    public static void main(String[] args) {
-        String file_path = "C:\\Users\\dtms\\Documents\\names.txt";
+    public static void main(String[] args) throws IOException {
+        FileInputStream inputStream = null;
+        Scanner sc = null;
+        String file_path = null;
+
+        file_path = "C:\\Users\\dtms\\Documents\\names.txt";
         File name_record_file = new File(file_path);
 
-        Levenshtein levenshtein = new Levenshtein("Ala Ola Roman");
-
         try {
-            Scanner scanner = new Scanner(name_record_file);
+            inputStream = new FileInputStream(file_path);
+            sc = new Scanner(inputStream, "UTF-8");
+            Levenshtein levenshtein = new Levenshtein("Ala Ola Roman");
 
-            while(scanner.hasNextLine()){
-                String name_record = scanner.nextLine();
+            while(sc.hasNextLine()){
+                String name_record = sc.nextLine();
                 System.out.println(levenshtein.calculate(name_record));
+            }
+
+            if (sc.ioException() != null) {
+                throw sc.ioException();
             }
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (sc != null) {
+                sc.close();
+            }
+        }
+        
 
     }
 }
